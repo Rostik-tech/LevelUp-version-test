982c87f761adb7f71a2f67f70df2b95546904961
Rost-tech <rostikpli44@gmail.com>
2026-02-21 18:18:39 +0200

    Add admin role middleware and secure admin access

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,9 @@ import authRouter from "./routes/auth.js";
 import orderRouter from "./routes/orders.js";
 import productRouter from "./routes/products.js";
 import paymentRouter from "./routes/payments.js";
-
 import { sequelize } from "./models/index.js";
+import { authenticateToken } from "./middleware/authMiddleware.js";
+import { isAdmin } from "./middleware/adminMiddleware.js";
 
 
 
@@ -23,6 +24,9 @@ app.use("/api/auth", authRouter);
 app.use("/api/orders", orderRouter);
 app.use("/api/products", productRouter);
 app.use("/api/payments", paymentRouter);
+app.get("/api/test-admin", authenticateToken, isAdmin, (req, res) => {
+  res.json({ message: "Admin access granted" });
+});
 
 // Запуск сервера
 const PORT = process.env.PORT || 5000;
