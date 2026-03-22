f1440ea1fc77f7268c1867150c4c2cf5d206f08c
Rost-tech <rostikpli44@gmail.com>
2026-03-22 23:43:36 +0200

    fix: use railway DATABASE_URL

--- backend/models/index.js
+++ backend/models/index.js
@@ -9,16 +9,17 @@ dotenv.config();
    SEQUELIZE INIT
 ====================== */
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST || "localhost",
-    dialect: "postgres",
-    logging: false,
-  }
-);
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 /* ======================
    IMPORT MODEL FACTORIES
